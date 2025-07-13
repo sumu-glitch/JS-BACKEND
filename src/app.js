bf5ee01317414aeb63d5abce9f73da6ebe8bc362
sumu-glitch <gauravparwate@gmail.com>
2025-07-13 16:02:38 +0530

    utils code

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import cors from "cors"
+import cookieParser from 'cookie-parser';
+
+const app = express();
+
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    Credential:true
+}))
+app.use(express.json({limit:"15kb"}))//form bharane ke wakt data ata haia isaliye.
+app.use(express.urlencoded({extended:true,limit:"15kb"}))//url ke data ke liye.
+app.use(express.static("public"))//static ye sirf files, folders, ya koi bhi images ke liye store ke liye hai
+app.use(cookieParser())//server me user ke cookie ki CRUD Operation ke liye hai
+
+export { app }
---
+++ src/utils/AsyncHandler.js
@@ -0,0 +1,35 @@
+//ye ( promices ) vala hai
+
+const asyncHandler = (func) => {
+    (req,res,next)=>{
+    Promise
+    .resolve(func(req,res,next))
+    .catch((err)=>{
+    })
+}
+}
+
+
+
+
+
+export {asyncHandler}
+
+
+
+//ye (async-await) try-catch block vala hai
+/*
+const asyncHandler = (func) => async (req,res,next) => {
+try {
+    await func(req,res,next)
+} catch (error) {
+    res
+    .status(error.code || 500)
+    .json({
+        success:false,
+        message:err.message
+    })
+}
+}
+
+*/
